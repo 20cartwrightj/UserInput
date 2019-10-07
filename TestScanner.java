10856a1d6da890fe20c157a2c4b1dd10e793ad4d
Jack Cartwright <20cartwrightj@flintridgeprep.org>
2019-10-07 13:45:00 -0700

    So far, I have finished the program 1.1. The program will print out the number that you type in in void main.

---
+++ TestScanner.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+class TestScanner {
+    public static void main(String[] args) {
+    Scanner consoleInput = new Scanner(System.in);
+    System.out.print("Please type your favorite number: ");
+    int favNumber = consoleInput.nextInt();
+}
+}
